85ccd3ddc8f1c2c4bb8a85d0865526fb767a8b9b
Sethiya Peellapitiya <sethiya.peellapitiya@kodez.com.au>
2022-12-06 12:52:07 +0530

    installed necessary packages

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');       //environmental variables
+const cors = require('cors');           //middleware
+const bodyParser = require('body-parser');   
+const { dbConnect } = require("./src/config/dbConnect");
+
+//import APIs
+//const TaskAPI = require('./apis/task.api');
+// const categoryAPI = require('./src/api/category.api');
+
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 4445;
+
+//root route
+app.route('/').get((req, res) => {
+  res.send('Attendance System - OnTime');
+});
+
+//register router - CHANGEABLE
+// app.use('/task', TaskAPI());
+// app.use('/category', categoryAPI());
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-  });
+  dbConnect();
+  console.log(`Server is up and running on PORT ${PORT}`);
+});
