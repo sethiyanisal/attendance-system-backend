0966de7c2c133be9f8753e8d09a49787b45519d1
sethiyanisal <sethiya.peellapitiya@kodez.com.au>
2022-12-23 16:54:02 +0530

    added the leave request model

---
+++ src/models/leaverequest.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const leaveSchema = new mongoose.Schema({
+    leavetype: { type: String, required: true },
+    dateFrom: { type: String, required: true },
+    dateTo: { type: String, required: true },
+    subject: { type: String, required: true },
+    postedBy: {type: mongoose.Schema.Types.ObjectId, ref:"users"}
+  });
+
+const Leave = mongoose.model("leaverequests", leaveSchema);
+
+module.exports = Leave;
